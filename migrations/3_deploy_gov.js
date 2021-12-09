2448f9460ff72ce4533e36be17b39cc7eb27b367
jupiter229 <smartwang1004@outlook.com>
2021-12-09 09:12:12 -0800

    finish gov migration file

--- migrations/3_deploy_gov.js
+++ migrations/3_deploy_gov.js
@@ -1,4 +1,11 @@
-const Governance... = artifacts.require('Governance...');
+const GovernanceToken = artifacts.require('GovernanceToken');
 module.exports = async function (deployer) {
-   await deployer.deploy(Governance..., constructor, params, comma, sepparated);
+   await deployer.deploy(GovernanceToken, 
+     "testoken", 
+     "TST",
+     "1000000000000000000000000",
+     "1000000000000000000000000",
+     "0",
+     "500"
+    );
 }
